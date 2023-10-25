8da9465f8bb765b839cb83336a710451816db618
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-10-25 19:55:55 -0300

    add emailJS

--- portfolio-perfumo/src/components/Contact.js
+++ portfolio-perfumo/src/components/Contact.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "../static/css/contact.css";
 
 const Contact = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_tkrdj5r', 'template_xliw3nb', form.current, 'qBv1F8H1XaE5Nej-0')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+
     return (
 <section id="contact">
     <div className="contact-container">
@@ -13,11 +28,11 @@ const Contact = () => {
                 </div>
             </div>
             <div className="col-md-6">
-                <form className="contact-form">
-                    <input className="form-input" type="text" placeholder="Your Name" />
-                    <input className="form-input" type="text" placeholder="Your Email" />
-                    <textarea className="form-input" placeholder="Your Message"></textarea>
-                    <button className="form-button">Send message</button>
+                <form ref={form} onSubmit={sendEmail} className="contact-form">
+                    <input className="form-input" type="text" placeholder="Tu Nombre"  name="user_name"/>
+                    <input className="form-input" type="email" placeholder="Tu Email" name="user_email"/>
+                    <textarea className="form-input" placeholder="Tu Mensaje" name="message"></textarea>
+                    <input type='submit' className="form-button" value="Send" />
                 </form>
             </div>
         </div>
